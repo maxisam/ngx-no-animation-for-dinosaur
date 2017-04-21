e6f312c39332c016ac283fd1248aed5eafd45e06
Sam Lin <maxisam@gmail.com>
2017-04-21 15:55:04 -0500

    fix: browser detect on IE9

--- src/browser-detect.ts
+++ src/browser-detect.ts
@@ -1,12 +1,14 @@
 export function isBrowserSupport(ua: string): boolean {
     if (!!window['chrome'] && !!window['chrome']['webstore']) {
         return true;
+    } else if (ua.indexOf('MSIE') !== -1) {
+        return ua.indexOf('MSIE 10') !== -1; // filter out all IE but IE10
+    } else if (ua.indexOf('Edge') || ua.indexOf('Trident')) {
+        return true; // IE11 and Edge
     } else if (ua.indexOf('Firefox') !== -1) {
         const match = /((?!Gecko.+)Firefox|Gecko(?!.+Firefox))(?: |\/)([\d]+)/.exec(ua);
         const ver = +match[2];
         return !!match[1] && !isNaN(ver) && ver >= 27; // Firefox 27+
-    } else if (ua.indexOf('MSIE 10') !== -1 || ua.indexOf('Trident/') !== -1 || ua.indexOf('Edge') !== -1) {
-        return true; // IE 10+, Edge
     } else if (ua.indexOf('Safari') !== -1) {
         let match = /(?:iPod|iPhone|iPad).+Version\/(\d+\.\d+).*Safari/.exec(ua); // iOS safari 7.1+
         let ver = match ? +match[1] : 0;
